f8c3564371cbc0848152d97b62ced17b523f2581
Maria Arias <majobel10@hotmail.com>
2020-10-12 13:35:24 -0500

    Updates

---
+++ precache-manifest.ab4bbd851483a0a51bfb18b0741691ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776aff5d5b9cf0384f63fdf575e58398",
+    "url": "/AriasPortafolio/index.html"
+  },
+  {
+    "revision": "3a16eaa4c14556dc42a5",
+    "url": "/AriasPortafolio/static/css/main.95e5718a.chunk.css"
+  },
+  {
+    "revision": "bec814eedf6067a41be3",
+    "url": "/AriasPortafolio/static/js/2.6ad7d95a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/AriasPortafolio/static/js/2.6ad7d95a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a16eaa4c14556dc42a5",
+    "url": "/AriasPortafolio/static/js/main.94be1e8c.chunk.js"
+  },
+  {
+    "revision": "5f06d60f789a9996fcab",
+    "url": "/AriasPortafolio/static/js/runtime-main.4684334f.js"
+  }
+]);
